7e46172c3e12d353f6ac6d49c3045b17759b0b10
Vithurshan2002 <sangervithu2002@gmail.com>
2025-09-30 21:25:26 +0530

    forgot,reset password functioality added

--- Controllers/UserRegistrationController.js
+++ Controllers/UserRegistrationController.js
@@ -1,10 +1,11 @@
 const userDetailmodel = require("../models/UserInforation");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 //register the student
 exports.UserRegister = async (req, res, next) => {
   const { Firstname, Lastname, email, password } = req.body;
   try {
-    if (Firstname && Lastname  && email && password) {
+    if (Firstname && Lastname && email && password) {
       const user = await userDetailmodel.findOne({ email: email });
       if (!user) {
         const userdata = await userDetailmodel.create({
@@ -14,20 +15,27 @@ exports.UserRegister = async (req, res, next) => {
           password: password,
         });
         console.log(userdata);
-        res.status(200).json({success:true, message: "Successfully Registered." });
+        res
+          .status(200)
+          .json({ success: true, message: "Successfully Registered." });
       } else {
         res.status(400).json({
-          success:false,
+          success: false,
           message: `${email} is alreay Exist.`,
         });
       }
     } else {
-      res.status(400).json({success:false,  message: "Some Informations is not provided" });
+      res
+        .status(400)
+        .json({ success: false, message: "Some Informations is not provided" });
     }
   } catch (error) {
     res
       .status(400)
-      .json({ success:false, message: `Unsuccessfull Registration :${error.message}` });
+      .json({
+        success: false,
+        message: `Unsuccessfull Registration :${error.message}`,
+      });
   }
 };
 
@@ -76,3 +84,73 @@ exports.UserLogin = async (req, res, next) => {
   }
 };
 
+//Forgotpassword
+exports.Forgotpassword = async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    if (email) {
+      const user = await userDetailmodel.findOne({ email: email });
+      if (!user) {
+        return res.status(404).json({ message: "Email is not Exist" });
+      }
+
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.USER,
+          pass: process.env.pass,
+        },
+      });
+
+      const tokens = jwt.sign({ email: email }, process.env.new_Secreat_key, {
+        expiresIn: "4m",
+      });
+      let mailOptions = {
+        from: process.env.USER,
+        to: email,
+        subject: "Forgotpassword",
+        text: `http://localhost:5173/resetPassword/${tokens}`,
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          return res.status(400).json({ message: error.message });
+        } else {
+          return res.status(200).json({ message: "Email Sent" });
+        }
+      });
+    } else {
+      res.status(404).json({
+        message: "Enter the Email",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//reset password
+
+exports.Resetpassword = async (req, res, next) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  try {
+    const decodedata = jwt.verify(token, process.env.new_Secreat_key);
+
+
+    const user = await userDetailmodel.findOneAndUpdate(
+      { email: decodedata.email },
+      { password: password },
+      {new: true}
+    );
+    if (user) {
+       res.status(200).json({ message: "succesfully Password Reset " });
+    }
+    else{
+       return res.status(400).json({ message:"Unauthorized Access" });
+    }
+
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { UserRegister, UserLogin, AccessDashboard } = require("../Controllers/UserRegistrationController");
+const { UserRegister, UserLogin, AccessDashboard, Forgotpassword, Resetpassword } = require("../Controllers/UserRegistrationController");
 const { varifyuser } = require("../middlewares/JWTmiddleware");
 const { getReviews } = require("../Controllers/UserReviewcontroler");
 const router = express.Router();
 
 router.post("/Register", UserRegister);
 router.post("/login",UserLogin)
+router.post("/forgotpassword",Forgotpassword)
+router.post("/resetpassword/:token",Resetpassword)
 /* router.get('/dashboard',varifyuser,AccessDashboard) */
 
 
