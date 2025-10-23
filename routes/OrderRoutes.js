682573f93320c3441d037b533e9909dddc34f680
Vithurshan2002 <sangervithu2002@gmail.com>
2025-10-23 20:34:46 +0530

    UPDATED

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -1,18 +1,21 @@
-const express=require('express');
-const { Addorder, getallOrder, updateOrderState } = require('../Controllers/OrderController');
-const { varifyuser } = require('../middlewares/JWTmiddleware');
-const router=express.Router();
+const express = require("express");
+const {
+  Addorder,
+  getallOrder,
+  updateOrderState,
+} = require("../Controllers/OrderController");
+const { varifyuser } = require("../middlewares/JWTmiddleware");
+const router = express.Router();
 
 //add the order
-router.post("/addOrder", Addorder)
-
+router.post("/addOrder", varifyuser, Addorder);
 
 //for admin
 
-//get the all order  
-router.get("/admin/getallorder",getallOrder);
+//get the all order
+router.get("/admin/getallorder", getallOrder);
 
 //change the order status
-router.put("/admin/updateorder/:id",updateOrderState);
+router.put("/admin/updateorder/:id", updateOrderState);
 
-module.exports=router;
+module.exports = router;
